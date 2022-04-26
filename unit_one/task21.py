03e04c626cf9795d8c5a168ac51d2b298cd1ba91
larsen87 <kovsesta@gmail.com>
2022-04-26 15:42:04 +0300

    task21

---
+++ unit_one/task21.py
@@ -0,0 +1,42 @@
+#todo: Задан словарь, его значения необходимо внести по соответвющим тегам и атрибутам вместо вопросов (?)
+# заполненный шаблон записать в файл index.html
+
+page = {"title": "Тег BODY",
+        "charset": "utf-8",
+        "alert": "Документ загружен",
+        "p": "Ut wisis enim ad minim veniam,  suscipit lobortis nisl ut aliquip ex ea commodo consequat."}
+
+
+template = """ 
+<!DOCTYPE HTML>
+<html> <head>
+  <title> ? </title>
+  <meta charset=?>
+ </head>
+ <body onload="alert(?)">
+ 
+  <p>?</p>
+
+ </body>
+</html>
+"""
+list_html = []
+index = []
+f = open("index.html", mode="w+", encoding="UTF-8")
+f.writelines(template)
+
+f = open("index.html", mode="r+t", encoding="UTF-8")
+
+list_html = f.readlines() # Записываю в список файл по строчно
+
+for key, val in page.items(): # Итерация словаря
+        for i in list_html: # Итерируем строки из списка строк
+                if key in i: # Если ключ есть в строке, то:
+                        x = list_html.index(i) # Записал номер строки в которой есть ключ
+                        i = i.replace("?", val) # Меняем в строке символ ? на значение из словаря
+                        list_html[x] = i # Перезаписал строку в списке со значением val
+
+f.seek(0,0)
+f.writelines(list_html)
+
+f.close()
