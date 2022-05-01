1dba67d5bab4e80ad684bd1975f796b74065b704
larsen87 <kovsesta@gmail.com>
2022-05-01 22:07:47 +0300

    task24

--- unit_one/task24.py
+++ unit_one/task24.py
@@ -6,15 +6,15 @@
 #8 5 12 12 15	                    hello
 #8 5 12 12 15 , 0 23 15 18 12 4 !	hello, world!
 
-alphabet = " abcdefghijklmnopqrstuvwxyz"
+alphabet = " abcdefghijklmnopqrstuvwxyz" # Алфавит, в индексе 0 записан пробел
 txt = input("Введите сообщение для конвертации: ")
 discription = ""
-input_list = txt.split(" ")
+input_list = txt.split(" ") # В список записываю значения разделенные пробелом, удаляя пробелы
 
-for letter in input_list:
-    if letter.isdigit():
-        n = int(letter)
-        discription = discription + alphabet[n]
+for letter in input_list: # Перебираю список
+    if letter.isdigit(): # если значение в списке цифра, то
+        n = int(letter) # то конвертирую в тип int
+        discription = discription + alphabet[n] # Добавляю в строку значение из алфавита по индексу "n"
     else:
-        discription = discription + letter
+        discription = discription + letter # Если значение не цифра и не пробел, то записываю, как есть
 print(discription)
